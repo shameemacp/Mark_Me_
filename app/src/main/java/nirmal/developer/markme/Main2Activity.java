76c5f77735eda63c2846a0c231e9488d9ff361c7
shameemacp <shameemacp091@gmail.com>
2019-11-05 11:43:09 +0530

    project

--- app/src/main/java/nirmal/developer/markme/Main2Activity.java
+++ app/src/main/java/nirmal/developer/markme/Main2Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -28,16 +29,20 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import cdflynn.android.library.checkview.CheckView;
+
 public class Main2Activity extends AppCompatActivity {
     String n, r;
     private IntentIntegrator qrScan;
+    MediaPlayer mp;
 
     SharedPreferences sharedPreferences;
-
+CheckView m;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        m=findViewById(R.id.check);
         sharedPreferences = getSharedPreferences("My", Context.MODE_PRIVATE);
         qrScan = new IntentIntegrator(this);
         qrScan.initiateScan();
@@ -56,12 +61,16 @@ public class Main2Activity extends AppCompatActivity {
             }
             if (result.getContents().equals("inet")) {
                 String currentTime = new SimpleDateFormat("mm", Locale.getDefault()).format(new Date());
-                Toast.makeText(Main2Activity.this, currentTime, Toast.LENGTH_SHORT).show();
-                if ((Integer.parseInt(currentTime) > 45)) {
-                    StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://appealable-merchant.000webhostapp.com/MarkMeMsg.php",
+
+               // Toast.makeText(Main2Activity.this, currentTime, Toast.LENGTH_SHORT).show();
+                if (Integer.parseInt(currentTime) < 45) {
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://appealable-merchant.000webhostapp.com/MarkMe.php",
                             new Response.Listener<String>() {
                                 @Override
                                 public void onResponse(String response) {
+                                    m.check();
+mp=MediaPlayer.create(Main2Activity.this,R.raw.apple);
+mp.start();
 
 
                                     Toast.makeText(Main2Activity.this, response, Toast.LENGTH_LONG).show();
@@ -78,27 +87,30 @@ public class Main2Activity extends AppCompatActivity {
                         @Override
                         protected Map<String, String> getParams() throws AuthFailureError {
                             Map<String, String> params = new HashMap<>();
-                            params.put(" Name", n);
-                            params.put(" regNo", r);
+                            params.put("name",sharedPreferences.getString("nameKey", "ji"));
+                            params.put("regno", sharedPreferences.getString("regKey", null));
                             return params;
                         }
 
                     };
                     RequestQueue requestQueue = Volley.newRequestQueue(Main2Activity.this);
                     requestQueue.add(stringRequest);
-                } else {
+                }
+                else
+                    {
 
 
-                    StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://appealable-merchant.000webhostapp.com/MarkMe.php",
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://appealable-merchant.000webhostapp.com/MarkMeMsg.php",
                             new com.android.volley.Response.Listener<String>() {
                                 @Override
                                 public void onResponse(String response) {
-                                    String time = "45";
+                                    //String time = "45";
+                                    Toast.makeText(Main2Activity.this,response,Toast.LENGTH_LONG).show();
                                     try {
                                         JSONArray jsonArray = new JSONArray(response);
                                         for (int i = 0; i < jsonArray.length(); i++) {
                                             JSONObject json_obj = jsonArray.getJSONObject(i);
-                                            time = json_obj.getString("Time");
+                                           // time = json_obj.getString("Time");
 
 
                                         }
@@ -106,7 +118,7 @@ public class Main2Activity extends AppCompatActivity {
                                         e.printStackTrace();
                                     }
 
-                                    Toast.makeText(Main2Activity.this, time, Toast.LENGTH_LONG).show();
+                                    //Toast.makeText(Main2Activity.this, time, Toast.LENGTH_LONG).show();
 
 
                                 }
